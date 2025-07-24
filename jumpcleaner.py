b90b02736989c77f1fdfae59dd49a31a36825852
eric <ericmoo@cisco.com>
2025-07-24 13:13:57 -0400

    Created Mac function to clear jump server files.

---
+++ jumpcleaner.py
@@ -0,0 +1,23 @@
+import os
+import platform
+
+
+def windows_cleaner():
+    pass
+
+
+def mac_cleaner():
+    from pathlib import Path
+    os.chdir('/Users/ericmoo/Downloads')
+    cwd = Path.cwd()
+    for file in cwd.rglob("*.rdp"):
+        file.unlink()
+    for file in cwd.rglob("*.ica"):
+        file.unlink()
+
+
+if __name__ == '__main__':
+    if platform.system() == 'Windows':
+        pass
+    if platform.system() == 'Darwin':
+        mac_cleaner()

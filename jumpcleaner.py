a713bc5a96ca35f8a982e56b0e403650147443dd
eric <ericmoo@cisco.com>
2025-07-26 06:00:33 -0400

    eliminated try/except

--- jumpcleaner.py
+++ jumpcleaner.py
@@ -19,24 +19,17 @@ def uni_cleaner():
         Returns:
             None
     """
-    try:
-        cwd = Path(os.path.join(os.path.expanduser("~"), "Downloads"))
-        rdp_files = list((cwd).glob("*.rdp"))
-        for file in rdp_files:
-            file.unlink()
-        ica_files = list((cwd).glob("*.ica"))
-        for file in ica_files:
-            file.unlink()
-        cr_files = list((cwd).glob("*.crdownload"))
-        for file in cr_files:
-            file.unlink()
 
-
-    except Exception as e:
-        with open("/tmp/error_log.txt", "a") as error_log:
-            error_log.write(f"Error occurred at {datetime.now()}:\n")
-            error_log.write(traceback.format_exc() + "\n")
-
+    cwd = Path(os.path.join(os.path.expanduser("~"), "Downloads"))
+    rdp_files = list((cwd).glob("*.rdp"))
+    for file in rdp_files:
+        file.unlink()
+    ica_files = list((cwd).glob("*.ica"))
+    for file in ica_files:
+        file.unlink()
+    cr_files = list((cwd).glob("*.crdownload"))
+    for file in cr_files:
+        file.unlink()
 
 
 if __name__ == '__main__':
